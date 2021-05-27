a079a18a0249814fe67a0e5ad091f8265008165f
Kusala177 <84542105+Kusala177@users.noreply.github.com>
2021-05-26 20:30:59 -0500

    Update App.java
    test pipeline commit

--- src/main/java/com/kusala/App.java
+++ src/main/java/com/kusala/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World test github hook!" );
+        System.out.println( "Hello World test github hook pipleline test now!" );
     }
 }
